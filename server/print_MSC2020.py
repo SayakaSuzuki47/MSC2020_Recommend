1e1ee3d32acb69955f9881f312f6fb077a156a86
Sayaka Suzuki <s1suzukisayaka1s@gmail.com>
2023-04-26 18:47:49 +0900

    Update print_MSC2020.py

--- server/print_MSC2020.py
+++ server/print_MSC2020.py
@@ -4,7 +4,7 @@ def print_msc2020(lst,msc_array):
         print("Alert! : Please include words that are characteristic of the sentence.")
     else:
         for line in lst:
-            msc_num, num = line
+            (msc_num, num) = line
             msc_name = msc_array[msc_array[:,0] == msc_num]
             sentence = msc_num+' : ' + msc_name + ' (The frequency is '+ num'.)'
             print(sentence)
