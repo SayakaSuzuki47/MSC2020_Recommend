290c5ed09006adc2258812428885d68903b7ceff
Sayaka Suzuki <s1suzukisayaka1s@gmail.com>
2023-04-26 18:49:00 +0900

    Update print_MSC2020.py

--- server/print_MSC2020.py
+++ server/print_MSC2020.py
@@ -6,5 +6,5 @@ def print_msc2020(lst,msc_array):
         for line in lst:
             (msc_num, num) = line
             msc_name = msc_array[msc_array[:,0] == msc_num]
-            sentence = msc_num+' : ' + msc_name + ' (The frequency is '+ num'.)'
+            sentence = msc_num+' : ' + msc_name + ' (The frequency is '+ num + '.)'
             print(sentence)
