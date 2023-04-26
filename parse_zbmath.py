df3243ca03870b52d5306e62bd06aa9067c18c55
Sayaka Suzuki <s1suzukisayaka1s@gmail.com>
2023-04-26 16:26:47 +0900

    Update parse_zbmath.py

--- parse_zbmath.py
+++ parse_zbmath.py
@@ -1,7 +1,15 @@
 import requests
 import re
+import xml.etree.ElementTree as ET
 
-def make_url_search_keyword(keyword):
+def make_keyword_msc2020_list(keyword):
     keyword_for_url = keyword.replace(" ", "%20")
     url = 'https://oai.zbmath.org/v1/helper/filter?filter=keyword%3A'+ keyword_for_url + '&metadataPrefix=oai_zb_preview'
-    return url
+    headers = {'accept': 'text/xml'}
+    response = requests.get(url, headers=headers)
+    root = ET.fromstring(response.text)
+    classifications = []
+    for classification in root.iter('{https://zbmath.org/zbmath/elements/1.0/}classification'):
+        classifications.append(classification.text)
+    
+    return classifications 
