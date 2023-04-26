0cecc7ca0fa20e66d966a8a1abb551da086acf3c
Sayaka Suzuki <s1suzukisayaka1s@gmail.com>
2023-04-26 16:10:36 +0900

    Update parse_zbmath.py

--- parse_zbmath.py
+++ parse_zbmath.py
@@ -3,5 +3,5 @@ import re
 
 def make_url_search_keyword(keyword):
     keyword_for_url = keyword.replace(" ", "%20")
-    url = 'https://oai.zbmath.org/v1/helper/filter?filter=keyword'+ keyword_for_url + '&metadataPrefix=oai_zb_preview'
+    url = 'https://oai.zbmath.org/v1/helper/filter?filter=keyword%3A'+ keyword_for_url + '&metadataPrefix=oai_zb_preview'
     return url
