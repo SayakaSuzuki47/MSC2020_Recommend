297b311689b4d576078ec282d5a42bd46b7947f6
Sayaka Suzuki <105976059+SayakaSuzuki47@users.noreply.github.com>
2023-05-02 00:37:50 +0900

    test

--- server/parse_zbmath.py
+++ server/parse_zbmath.py
@@ -6,10 +6,8 @@ from collections import Counter
 def make_keyword_msc2020_list(keyword):
     keyword_for_url = keyword.replace(" ", "%20")
     url = 'https://oai.zbmath.org/v1/helper/filter?filter=keyword%3A'+ keyword_for_url + '&metadataPrefix=oai_zb_preview'
-    #headers = {'accept': 'text/xml'}
     params = {'accept': 'text/xml'}
     response = requests.get(url, params=params)
-    #response = requests.get(url, headers=headers)
     root = ET.fromstring(response.text)
     
     classifications = []
